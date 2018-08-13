2708d15977739c984e9acaaa9098ac310fde85fe
Essam Ayari <ayariissam@gmail.com>
2018-08-13 10:20:20 +0100

    add the configurations file

---
+++ config-sample.js
@@ -0,0 +1,15 @@
+const config = {
+    app: {
+        mode: 'DEV',
+        port: 5000
+    },
+    database: {
+        host: 'localhost',
+        name: 'saraha',
+        port: 27017,
+        username: '',
+        password: ''
+    }
+}
+
+module.exports = config;
