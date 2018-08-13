cc07a109100b1ef8dbcf28f2f9a01375b61353c0
Essam Ayari <ayariissam@gmail.com>
2018-08-13 10:46:59 +0100

    connect to the database

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
+const config = require('./config');
+const mongoose = require('mongoose');
+
+let connectionString = '';
+if(config.database.credentials) {
+    connectionString = 'mongodb://'+ config.database.username +':'+ config.database.password +'@'+ config.database.host +':'+ config.database.port +'/' + config.database.name;
+} else {
+    connectionString = 'mongodb://'+ config.database.host +':'+ config.database.port +'/' + config.database.name;
+}
+mongoose.connect(connectionString);
+
+mongoose.connection.on('connected', ()=>{
+    console.log('Connected to: ' + config.database.name);
+});
+mongoose.connection.on('error', (err)=>{
+    console.log('Database Error: ' + err);
+});
 
 const app = express();
 
@@ -12,7 +29,7 @@ app.use(cors());
 // Setting the static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(5000, () => {
-    console.log('Server listenig on port 5000');
+app.listen(config.app.port, () => {
+    console.log('Server listenig on port: '+ config.app.port);
 })
 
